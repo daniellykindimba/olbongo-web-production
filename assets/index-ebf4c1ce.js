43823925f6f1b61faeaf2c290dc949e3f2afc655
Daniel Peter Kindimba <daniellykindimba@gmail.com>
2023-12-05 06:54:11 +0300

    ave

---
+++ assets/index-ebf4c1ce.js
@@ -0,0 +1,1 @@
+import{j as e,bt as r,f5 as t}from"./index-6e39ecf9.js";import{B as s}from"./Breadcrumb-23e6902d.js";const i=()=>e.jsxs(e.Fragment,{children:[e.jsxs(s,{children:[e.jsx(s.Item,{children:e.jsx(r,{to:"/odrive",children:"Home"})}),e.jsx(s.Item,{children:"Files"})]}),e.jsx(t,{})]}),m=()=>e.jsx(i,{});export{m as FilesPage};
