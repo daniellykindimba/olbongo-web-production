0a49c6719d4825ec37c20cc222396da71ee21176
Daniel Peter Kindimba <daniellykindimba@gmail.com>
2023-12-11 10:46:13 +0300

    save

---
+++ assets/index-9b13e950.js
@@ -0,0 +1,1 @@
+import{j as e,bt as r,f5 as t}from"./index-afb01353.js";import{B as s}from"./Breadcrumb-835bbd39.js";const i=()=>e.jsxs(e.Fragment,{children:[e.jsxs(s,{children:[e.jsx(s.Item,{children:e.jsx(r,{to:"/odrive",children:"Home"})}),e.jsx(s.Item,{children:"Files"})]}),e.jsx(t,{})]}),m=()=>e.jsx(i,{});export{m as FilesPage};
