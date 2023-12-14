b2a2e25d2e313e936fedb74e83080493df0409a9
Daniel Peter Kindimba <daniellykindimba@gmail.com>
2023-12-14 07:28:20 +0300

    updates

---
+++ assets/index-fab5fa08.js
@@ -0,0 +1,1 @@
+import{j as e,bt as r,f6 as t}from"./index-a5fcf18b.js";import{B as s}from"./Breadcrumb-b23f4465.js";const i=()=>e.jsxs(e.Fragment,{children:[e.jsxs(s,{children:[e.jsx(s.Item,{children:e.jsx(r,{to:"/odrive",children:"Home"})}),e.jsx(s.Item,{children:"Files"})]}),e.jsx(t,{})]}),m=()=>e.jsx(i,{});export{m as FilesPage};
